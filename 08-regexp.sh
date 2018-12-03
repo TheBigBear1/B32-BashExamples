038746bdb44d557de53e9b4f1e0be7e84da2c4ae
Charles Laganiere <laganierecharles@gmail.com>
2018-12-03 11:12:18 -0500

    Commentaire regxp

--- 08-regexp.sh
+++ 08-regexp.sh
@@ -3,7 +3,12 @@
 echo -n "Texte : "
 read texte
 
+# ~ : signifie que ce qui suit est une expression réguliaire
 if [[ $texte =~ ^[0-9]$ ]];then
 	echo "Numérique"
 fi
 
+# + : Si l'expression réguliaire se retrouve au moins une fois
+if [[ $texte=~ ^[0-9]+$ ]];then 
+	echo "Numérique"
+fi
