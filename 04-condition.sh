41922deaac0135b1a41369588ed2ac258ce971df
Charles Laganiere <laganierecharles@gmail.com>
2018-12-03 10:42:10 -0500

    Modification des conditions

--- 04-condition.sh
+++ 04-condition.sh
@@ -1,7 +1,34 @@
 #!/bin/bash
 
+read -p "Quel est votre note? " note
 
+if test $note -lt 60
+then 
+	echo "Désolé, vous deez reprendre le cours ..."
+elif [[ $note -eq 60 ]]
+then
+	echo "Vous êtes à la limite!!"
+else
+	echo "Bravo!"
+fi
 
+read lettre 
+
+case $lettre in
+#Lower est le cas qui dit : tous les lettres minuscules de a à z
+	[[:lower:]])
+		echo "minuscule"
+		;;
+	*)
+#L'étoile est le cas par défaut
+		echo "autre ..."
+		;;
+esac
+
+if test $lettre != "a"
+then 
+	echo "La lettre n'est pas a"
+fi
 
 
 
