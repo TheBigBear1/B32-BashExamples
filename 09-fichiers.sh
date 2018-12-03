f15373e34ec0dbf853debfe345eb536f4b37dda9
Charles Laganiere <laganierecharles@gmail.com>
2018-12-03 11:18:08 -0500

    Modification fichier

--- 09-fichiers.sh
+++ 09-fichiers.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+#Vérifie si le fichier existe vraiment
+# -e : vérifie si la variable test existe 
+if test -e $1
+then
+	echo "existe"
+
+# -d : dit si la variable test est un répertoire
+	if test -d $1
+	then
+		echo "Il est un répertoire"
+	fi
+
+else
+	echo "inexistant"
+fi
+
+
