0209e230034bd7cc7ec77ad08b2062807aa92e61
Charles Laganiere <laganierecharles@gmail.com>
2018-12-03 12:31:03 -0500

    Exercise 3 pas fini

--- exercice3.sh
+++ exercice3.sh
@@ -1,1 +1,25 @@
 #!/bin/bash
+
+space=0
+for((i=0;i<20;i++))
+do
+	(( $space+1))
+	for((j=0;j<$space;j++))
+	do
+	echo -n " "
+	done
+
+	if test $i = 19
+	then
+		echo -n "Boom"
+	else
+	echo -n "0"
+	fi
+
+	sleep 1
+done
+
+echo ""
+echo "Vous venez d'exploser"
+
+
