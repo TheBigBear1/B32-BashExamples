600d2ac0bbb196622a73cc03ea559dfd197f3550
Charles Laganiere <laganierecharles@gmail.com>
2018-12-03 10:54:18 -0500

    Modification boucle

--- 05-boucles.sh
+++ 05-boucles.sh
@@ -1,2 +1,29 @@
 #!/bin/bash
 
+for ((i=0;i<5;i++))
+do 
+	echo $i
+done 
+
+liste=`ls`
+
+for fic in $liste
+do
+	echo $fic
+done
+
+#Pour chacun des éléments de la liste, fait ....
+for fic in $liste
+do
+	echo $fic
+done
+
+resultat=o
+
+#-a = and
+#-o = or
+while test $resultat = o
+do
+	read -p "Entrez o pour répéter la boucle : " resultat
+done
+
