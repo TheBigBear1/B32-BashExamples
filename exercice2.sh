b655f448a5b2f531bd50fee7031f8630db53ce06
Charles Laganiere <laganierecharles@gmail.com>
2018-12-03 12:10:54 -0500

    Exercise 2 finalisé

--- exercice2.sh
+++ exercice2.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+
+option=z
+
+while test $option != "b" -a $option != "B"
+do
+echo "--------------------------------------------"
+echo "Choisissez parmis les deux otpions suivantes"
+echo "	a - Effectuer une recherche d'un mot"
+echo "	b - Quitter le programme"
+echo "--------------------------------------------"
+
+read -p "Quel est votre choix ? " option
+
+if test $option = "a" -o  $option = "A"
+then
+	./exercice1.sh
+else
+	echo "Fin du programme ... "
+fi
+done
