c59c3235d942b43247978310c5f84eda9adf2c7b
Charles Laganiere <laganierecharles@gmail.com>
2018-12-03 09:51:05 -0500

    Modification ficher hello_world

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+#Ceci est un commentaire
+echo "Salut"
+
+#-n empêche retour de ligne
+echo -n "Vive "
+echo "LInux"
+
+echo -e "123\n123\n123"
+
+read -p "Vous avez quel age? " age
+echo $age " ans! Vous paraissez plus jeune!"
+
