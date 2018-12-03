b4207408dddbec293d3fbdb61d8440bf9269b4c0
Charles Laganiere <laganierecharles@gmail.com>
2018-12-03 10:04:09 -0500

    Mise à jour variables

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
 
+prenom=KimCharles
+echo $prenom
+
+#Frocer une déclaration d'une variables en "int"
+declare -i age=23
+
+# Mettre une variables READ ONLY
+declare -r age2=33
+
+
+#Les variables système
+#Nom de l'exécutable
+echo $0
+#Argument 1
+echo $1
+#Argument 2
+echo $2
+#Le nombre d'argument
+echo $#
